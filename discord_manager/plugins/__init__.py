3df2e70771510b7b8d2bd18b1cd8e5cf7c81437c
eqwgeryfgbdf <0988288034sky@gmail.com>
2025-04-24 23:49:37 +0800

    Add initial project structure and core files for Discord bot management

--- discord_manager/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Plugin system initialization.
-This package contains the core plugin system and plugin base classes.
-"""
-
-from .base import BasePlugin
-from .manager import PluginManager
-
-__all__ = ['BasePlugin', 'PluginManager'] 
